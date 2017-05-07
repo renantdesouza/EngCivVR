df18b9734263b1a7daf36ee1722061e0fde0a86c
renantdesouza <renantdesouza@hotmail.com>
2017-05-07 15:43:50 -0300

    primeira tela

--- index.android.js
+++ index.android.js
@@ -1,53 +1,60 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+import React, {
+    Component
+} from 'react';
+import Native, {
+    View,
+    Text,
+    AppRegistry
 } from 'react-native';
+import Menu, {
+    MenuContext,
+    MenuOptions,
+    MenuOption,
+    MenuTrigger
+} from 'react-native-menu';
 
-export default class EngCivVR extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+export default class EngClivVR extends Component {
+    render() {
+        return (
+            <MenuContext style={styles.menu}>
+                <View style={styles.externalView}>
+                    <View style={styles.view}>
+                        <Text>
+                            VR App
+                        </Text>
+                    </View>
+                    <Menu onSelect={(value) => alert(`User selected the number ${value}`)}>
+                        <MenuTrigger>
+                            <Text style={styles.text}>&#8942;</Text>
+                        </MenuTrigger>
+                        <MenuOptions>
+                            <MenuOption value={1}>
+                                <Text>Criar Localidade</Text>
+                            </MenuOption>
+                        </MenuOptions>
+                    </Menu>
+                </View>
+                <View style={styles.view}><Text>Hello!</Text></View>
+            </MenuContext>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+const styles = Stylesheet.create({
+    menu: {
+        flex: 1
+    },
+    externalView: {
+        padding: 10,
+        flexDirection: 'row',
+        backgroundColor: 'pink'
+    },
+    view: {
+        flex: 1, justifyContent: 'center', alignItems: 'center'
+    },
+    text: {
+        fontSize: 20
+    }
 });
 
-AppRegistry.registerComponent('EngCivVR', () => EngCivVR);
+AppRegistry.registerComponent('EngCivVR', () => EngClivVR);
